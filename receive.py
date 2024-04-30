4fd177e6cbf48b877d661436060d15df0fc8d2b9
barzansaeedpour <barzansaeedpour@gmail.com>
2024-04-30 09:56:25 +0330

    adding the exchange

---
+++ receive.py
@@ -0,0 +1,59 @@
+################################# simple example
+# #!/usr/bin/env python
+# import pika, sys, os
+
+# def main():
+#     connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+#     channel = connection.channel()
+
+#     channel.queue_declare(queue='hello')
+
+#     def callback(ch, method, properties, body):
+#         print(f" [x] Received {body}")
+
+#     channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
+
+#     print(' [*] Waiting for messages. To exit press CTRL+C')
+#     channel.start_consuming()
+
+# if __name__ == '__main__':
+#     try:
+#         main()
+#     except KeyboardInterrupt:
+#         print('Interrupted')
+#         try:
+#             sys.exit(0)
+#         except SystemExit:
+#             os._exit(0)
+
+################################# Another example
+
+
+import pika
+import json
+
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+
+queue = channel.queue_declare('order_notify')
+queue_name = queue.method.queue
+
+channel.queue_bind(
+    exchange='order',
+    queue=queue_name,
+    routing_key='order.notify' # binding key
+)
+
+def callback(ch, method, properties, body):
+    payload = json.loads(body)
+    print('[x] Notifying {}'.format(payload['user_email']))
+    print('[x] Done')
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+    
+channel.basic_consume(on_message_callback=callback, queue=queue_name)
+
+print(' [*] Waiting for messages. To exit press CTRL+C')
+channel.start_consuming()
+
+
--- send.py
+++ send.py
@@ -1,4 +1,50 @@
-import pika
+######################## simple example
+# import pika
+# connection = pika.BlockingConnection(
+#     pika.ConnectionParameters(host='localhost'))
+# channel = connection.channel()
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+# channel.queue_declare(queue='hello')
+
+# channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
+# print(" [x] Sent 'Hello World!'")
+# connection.close()
+
+######################## Another example
+
+import pika
+import json
+import uuid
+
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='localhost'))
+
 channel = connection.channel()
+
+channel.exchange_declare(
+    exchange='order',
+    exchange_type='direct'
+)
+
+order = {
+    'id': str(uuid.uuid4()),
+    'user_email': 'barzansaeedpour@gmail.com',
+    'product': 'RabbitMQ',
+    'quantity': 1
+}
+
+channel.basic_publish(
+    exchange='order',
+    routing_key='order.notify',
+    body=json.dumps({'user_email': order['user_email']})
+)
+
+print('[x] Sent notify message')
+
+channel.basic_publish(
+    exchange='order',
+    routing_key='order.report',
+    body=json.dumps(order)
+)
+
+connection.close()
